f72f487ec927ec125198f425d9f6741f687b9c01
Kilo Loco <kiloloco@me.com>
2020-09-21 17:51:09 -0700

    swiftui-for-beginners start

---
+++ apple/swiftui-for-beginners/swiftui-for-beginners/swiftui_for_beginnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_for_beginnersApp.swift
+//  swiftui-for-beginners
+//
+//  Created by Kyle Lee on 9/21/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_for_beginnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
