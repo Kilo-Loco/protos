939ecf60ab82448bdcebcd98d4d615704da8f95d
Kilo Loco <kyle@kiloloco.com>
2021-01-28 14:41:51 -0800

    amplify-ios-multifactor-auth-proto init

---
+++ apple/amplify-ios-multifactor-auth-proto/amplify-ios-multifactor-auth-proto/amplify_ios_multifactor_auth_protoApp.swift
@@ -0,0 +1,17 @@
+//
+//  amplify_ios_multifactor_auth_protoApp.swift
+//  amplify-ios-multifactor-auth-proto
+//
+//  Created by Kilo Loco on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct amplify_ios_multifactor_auth_protoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
