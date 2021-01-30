f0ac6dbc3eafe759c66181d8362550f45baa3a9a
Kilo Loco <kyle@kiloloco.com>
2021-01-30 14:07:22 -0800

    remote-images-proto start

---
+++ apple/remote-image-proto/remote-image-proto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  remote-image-proto
+//
+//  Created by Kilo Loco on 1/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ apple/remote-image-proto/remote-image-proto/remote_image_protoApp.swift
@@ -0,0 +1,17 @@
+//
+//  remote_image_protoApp.swift
+//  remote-image-proto
+//
+//  Created by Kilo Loco on 1/30/21.
+//
+
+import SwiftUI
+
+@main
+struct remote_image_protoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
