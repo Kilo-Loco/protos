ea04ede5ef5e3d9c15248e4e31cf69f81e59ff17
Kilo Loco <kyle@kiloloco.com>
2021-02-09 14:19:38 -0800

    sqflite-data-table wip

---
+++ apple/links-in-text-proto/links-in-text-proto/links_in_text_protoApp.swift
@@ -0,0 +1,17 @@
+//
+//  links_in_text_protoApp.swift
+//  links-in-text-proto
+//
+//  Created by Kilo Loco on 2/9/21.
+//
+
+import SwiftUI
+
+@main
+struct links_in_text_protoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
