c40193ef2b5cc9ca5f7b60b2188ad1e098a3a698
Kilo Loco <kyle@kiloloco.com>
2021-02-24 10:03:53 -0800

    global_chat add send functionality

--- flutter/global_chat_proto/lib/main.dart
+++ flutter/global_chat_proto/lib/main.dart
@@ -1,5 +1,13 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:amplify_flutter/amplify.dart';
 import 'package:flutter/material.dart';
+import 'package:global_chat_proto/amplifyconfiguration.dart';
 import 'package:global_chat_proto/auth_view.dart';
+import 'package:global_chat_proto/models/ModelProvider.dart';
+
+import 'messages_view.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +19,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _username;
+  String _email;
+
+  @override
+  void initState() {
+    super.initState();
+    _configureAmplify().then((_) => _attemptAutoLogin());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +33,38 @@ class _MyAppState extends State<MyApp> {
         home: Navigator(
       pages: [
         MaterialPage(
-            child: AuthView(
-                didSignIn: (username) => setState(() => _username = username)))
+            child:
+                AuthView(didSignIn: (email) => setState(() => _email = email))),
+        if (_email != null) MaterialPage(child: MessagesView(email: _email))
       ],
+      onPopPage: (route, result) => route.didPop(result),
     ));
+  }
+
+  Future<void> _configureAmplify() async {
+    try {
+      await Amplify.addPlugins([
+        AmplifyAuthCognito(),
+        AmplifyDataStore(modelProvider: ModelProvider.instance),
+        AmplifyAPI()
+      ]);
+
+      await Amplify.configure(amplifyconfig);
+      print('configured');
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void _attemptAutoLogin() async {
+    try {
+      final attributes = await Amplify.Auth.fetchUserAttributes();
+      final email = attributes
+          .firstWhere((element) => element.userAttributeKey == 'email')
+          .value;
+      setState(() => _email = email);
+    } catch (e) {
+      print(e);
+    }
   }
 }
--- flutter/global_chat_proto/lib/messages_view.dart
+++ flutter/global_chat_proto/lib/messages_view.dart
@@ -1,25 +1,36 @@
+import 'package:amplify_datastore/amplify_datastore.dart';
+import 'package:amplify_flutter/amplify.dart';
 import 'package:flutter/material.dart';
 
 import 'models/Message.dart';
 
 class MessagesView extends StatefulWidget {
-  final String username;
+  final String email;
 
-  MessagesView({Key key, this.username});
+  MessagesView({Key key, this.email});
 
   @override
   State<StatefulWidget> createState() => _MessagesViewState();
 }
 
 class _MessagesViewState extends State<MessagesView> {
-  List<Message> messages;
+  List<Message> _messages = [];
   final _messageBodyController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _getMessages().then((value) => _observeMessages());
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
-      body: _messagesListView(),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [_messagesListView(), _createMessageContainer()],
+      ),
     );
   }
 
@@ -32,9 +43,9 @@ class _MessagesViewState extends State<MessagesView> {
 
   Widget _messagesListView() {
     return ListView.builder(
-        itemCount: messages.length,
+        itemCount: _messages.length,
         itemBuilder: (context, index) {
-          final message = messages[index];
+          final message = _messages[index];
           return ListTile(
             title: Text(message.sender),
             subtitle: Text(message.body),
@@ -46,15 +57,45 @@ class _MessagesViewState extends State<MessagesView> {
     return Container(
       color: Colors.white,
       padding: EdgeInsets.all(16),
-      child: Row(
-        children: [
-          Flexible(
-              child: TextField(
-            controller: _messageBodyController,
-            decoration: InputDecoration(hintText: 'Enter a message'),
-          ))
-        ],
+      child: SafeArea(
+        child: Row(
+          children: [
+            Flexible(
+                child: TextField(
+              controller: _messageBodyController,
+              decoration: InputDecoration(hintText: 'Enter a message'),
+            )),
+            TextButton(onPressed: _sendMessage, child: Text('Send'))
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> _getMessages() async {
+    try {
+      final messages = await Amplify.DataStore.query(Message.classType);
+      setState(() => _messages = messages);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void _observeMessages() {
+    final stream = Amplify.DataStore.observe(Message.classType);
+    stream.listen((event) => _getMessages());
+  }
+
+  void _sendMessage() async {
+    final newMessage = Message(
+        body: _messageBodyController.text,
+        sender: widget.email,
+        creationDate: TemporalDateTime.now());
+    try {
+      await Amplify.DataStore.save(newMessage);
+      _messageBodyController.text = '';
+    } catch (e) {
+      print(e);
+    }
   }
 }
