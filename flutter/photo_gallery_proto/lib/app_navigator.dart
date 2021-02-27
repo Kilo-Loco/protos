8a25b8f7f7dc220e8794576793b62c1750405ac3
Kilo Loco <kyle@kiloloco.com>
2021-02-27 10:14:03 -0800

    photo-gallery-app single navigator flow

--- flutter/photo_gallery_proto/lib/app_navigator.dart
+++ flutter/photo_gallery_proto/lib/app_navigator.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:photo_gallery_proto/auth_navigator.dart';
+import 'login_view.dart';
 
 class AppNavigator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Navigator(
-      pages: [MaterialPage(child: AuthNavigator())],
+      pages: [MaterialPage(child: LoginView())],
       onPopPage: (route, result) => route.didPop(result),
     );
   }
