0fd560597f0b02379e3e384499622111d3e154a0
Kilo Loco <kyle@kiloloco.com>
2021-03-02 16:27:49 -0800

    photo-gallery-proto update confirmation view renames

--- flutter/photo_gallery_proto/lib/confirmation_cubit.dart
+++ flutter/photo_gallery_proto/lib/confirmation_cubit.dart
@@ -1,6 +1,23 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:photo_gallery_proto/auth_repository.dart';
+
 enum ConfirmationState {
   initial,
   attemptingConfirm,
   confirmSuccess,
   confirmFailed
+}
+
+class ConfirmationCubit extends Cubit<ConfirmationState> {
+  final AuthRepository authRepo;
+
+  ConfirmationCubit({this.authRepo}) : super(ConfirmationState.initial);
+
+  void confirm({String username, String password}) async {
+    emit(ConfirmationState.attemptingConfirm);
+
+    // try {
+    //   await
+    // }
+  }
 }
--- flutter/photo_gallery_proto/lib/confirmation_view.dart
+++ flutter/photo_gallery_proto/lib/confirmation_view.dart
@@ -1,22 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ConfirmationView extends StatelessWidget {
+import 'auth_repository.dart';
+import 'confirmation_cubit.dart';
+import 'navigator_cubit.dart';
+
+class ConfirmationView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ConfirmationViewState();
+}
+
+class _ConfirmationViewState extends State<ConfirmationView> {
+  final _codeController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _appBar(),
+      body: SafeArea(
+        minimum: EdgeInsets.symmetric(horizontal: 20),
+        child: BlocProvider<ConfirmationCubit>(
+            create: (_) =>
+                ConfirmationCubit(authRepo: context.read<AuthRepository>()),
+            child: _confirmationPage()),
+      ),
     );
   }
 
-  Widget _appBar(BuildContext context) {
-    return AppBar(
-      backgroundColor: Colors.transparent,
-      elevation: 0,
-      leading: IconButton(
-        icon: Icon(Icons.cancel),
-        color: Colors.black,
-        onPressed: () => Navigator.of(context).pop(),
+  Widget _confirmationPage() {
+    return BlocListener<ConfirmationCubit, ConfirmationState>(
+      listener: (context, state) {
+        if (state == ConfirmationState.confirmFailed) {
+          Scaffold.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(
+                content: Text('Failed to Confirmation'),
+              ),
+            );
+        }
+      },
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          _confirmationForm(),
+          _showSignUpButton(),
+        ],
       ),
+    );
+  }
+
+  Widget _confirmationCodeField() {
+    return TextFormField(
+      controller: _codeController,
+      validator: (value) {
+        return value.length < 6 ? 'Invalid code' : null;
+      },
+      decoration: InputDecoration(
+          hintText: 'Confirmation code', icon: Icon(Icons.security)),
+    );
+  }
+
+  Widget _confirmationButton() {
+    return BlocBuilder<ConfirmationCubit, ConfirmationState>(
+      builder: (context, state) {
+        return state == ConfirmationState.attemptingConfirm
+            ? CircularProgressIndicator()
+            : ElevatedButton(
+                child: Text('Confirmation'),
+                onPressed: () =>
+                    BlocProvider.of<ConfirmationCubit>(context).confirm(
+                  username: _codeController.text,
+                ),
+              );
+      },
+    );
+  }
+
+  Widget _confirmationForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _confirmationCodeField(),
+          _confirmationButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _showSignUpButton() {
+    return TextButton(
+      onPressed: () => BlocProvider.of<NavigatorCubit>(context).showSignUp(),
+      child: Text('Don\'t have an account? Sign up.'),
     );
   }
 }
