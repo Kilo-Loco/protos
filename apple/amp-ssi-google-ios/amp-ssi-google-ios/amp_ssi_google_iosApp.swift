476a8e88ba38fd08bc03362be03c70de5a168e26
Kilo Loco <kyle@kiloloco.com>
2021-03-05 20:59:31 -0800

    amp-ssi-google-ios initial

---
+++ apple/amp-ssi-google-ios/amp-ssi-google-ios/amp_ssi_google_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  amp_ssi_google_iosApp.swift
+//  amp-ssi-google-ios
+//
+//  Created by Kilo Loco on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct amp_ssi_google_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
