94650fde3c0882749440c7eb47ba477ace5ce621
Kilo Loco <kyle@kiloloco.com>
2021-03-10 15:07:44 -0800

    amplify-social-sign-in finished

--- apple/siwa-swiftui-proto/siwa-swiftui-proto/siwa_swiftui_protoApp.swift
+++ apple/siwa-swiftui-proto/siwa-swiftui-proto/siwa_swiftui_protoApp.swift
@@ -5,6 +5,8 @@
 //  Created by Kilo Loco on 3/8/21.
 //
 
+import Amplify
+import AmplifyPlugins
 import AuthenticationServices
 import SwiftUI
 
@@ -33,8 +35,6 @@ class AuthControllerDelegate: NSObject, ObservableObject, ASAuthorizationControl
         else { return UIWindow() }
         return window
     }
-    
-    
 }
 
 @main
@@ -45,11 +45,23 @@ struct siwa_swiftui_protoApp: App {
     init() {
 //        getCredentialsState()
 //        existingAccountSetup()
+        configureAmplify()
     }
     
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+    
+    private func configureAmplify() {
+        do {
+            try Amplify.add(plugin: AWSCognitoAuthPlugin())
+            try Amplify.configure()
+            print("Successfully configured Amplify")
+            
+        } catch {
+            print("Could not configure Amplify", error)
         }
     }
     
