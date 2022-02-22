a5d62d44b6262e76cec997791a072c172ba83a4e
Kilo Loco <kyle@kiloloco.com>
2022-02-22 14:15:22 -0800

    initial custom-auth-flow-ios

---
+++ apple/Custom Auth Flow/Custom Auth Flow/Custom_Auth_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Auth_FlowApp.swift
+//  Custom Auth Flow
+//
+//  Created by Kilo Loco on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Auth_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
