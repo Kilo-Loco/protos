b0d926eefdc314e697afc313dc89361ccd63890a
Kilo Loco <kyle@kiloloco.com>
2022-02-22 14:13:27 -0800

    wip sms-sign-in-flutter

---
+++ flutter/sms_sign_in/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "smssigninb6ab3c90b6ab3c90": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    }
+}
