cef44359f3c876930bf09acecde93e0db80c7db4
Kilo Loco <kyle@kiloloco.com>
2022-05-27 21:11:19 -0700

    Initial Product Marketplace Proto

---
+++ apple/product-marketplace-proto/product-marketplace-proto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  product-marketplace-proto
+//
+//  Created by Kilo Loco on 5/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ apple/product-marketplace-proto/product-marketplace-proto/product_marketplace_protoApp.swift
@@ -0,0 +1,17 @@
+//
+//  product_marketplace_protoApp.swift
+//  product-marketplace-proto
+//
+//  Created by Kilo Loco on 5/27/22.
+//
+
+import SwiftUI
+
+@main
+struct product_marketplace_protoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
